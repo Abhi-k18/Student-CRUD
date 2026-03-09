08b14b95f60698bb689a75fd66b570e8c3282e19
Abhi-k18 <abhikoyappanavar18@gmail.com>
2026-03-10 00:41:15 +0530

    Initial commit

---
+++ student-backend/src/students/students.service.ts
@@ -0,0 +1,37 @@
+import { Injectable } from '@nestjs/common';
+import { InjectRepository } from '@nestjs/typeorm';
+import { Repository } from 'typeorm';
+import { Student } from './entities/student.entity';
+import { CreateStudentDto } from './dto/create-student.dto';
+import { UpdateStudentDto } from './dto/update-student.dto';
+
+@Injectable()
+class StudentsService {
+
+  constructor(
+    @InjectRepository(Student)
+    private studentRepository: Repository<Student>,
+  ) {}
+
+  create(createStudentDto: CreateStudentDto) {
+    const student = this.studentRepository.create(createStudentDto);
+    return this.studentRepository.save(student);
+  }
+
+  findAll() {
+    return this.studentRepository.find();
+  }
+
+  findOne(id: number) {
+    return this.studentRepository.findOne({ where: { id } });
+  }
+
+  update(id: number, updateStudentDto: UpdateStudentDto) {
+    return this.studentRepository.update(id, updateStudentDto);
+  }
+
+  remove(id: number) {
+    return this.studentRepository.delete(id);
+  }
+}
+export { StudentsService };
