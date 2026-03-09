d8796ca8250efff10bcceacd7b313122c0149e2c
Abhi-k18 <abhikoyappanavar18@gmail.com>
2026-03-10 02:03:35 +0530

    fix aiven postgres connection

--- student-backend/src/app.module.ts
+++ student-backend/src/app.module.ts
@@ -1,18 +1,24 @@
 import { Module } from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm';
+import { StudentsModule } from './students/students.module';
+import { Student } from './students/entities/student.entity';
 
 @Module({
   imports: [
     TypeOrmModule.forRoot({
       type: 'postgres',
-      host: 'localhost',
-      port: 5432,
-      username: 'postgres',
-      password: 'postgres',
-      database: 'studentdb',
-      autoLoadEntities: true,
+      host: process.env.DB_HOST,
+      port: parseInt(process.env.DB_PORT || '5432'),
+      username: process.env.DB_USERNAME,
+      password: process.env.DB_PASSWORD,
+      database: process.env.DB_NAME,
+      ssl: {
+        rejectUnauthorized: false,
+      },
+      entities: [Student],
       synchronize: true,
     }),
+    StudentsModule,
   ],
 })
 export class AppModule {}
